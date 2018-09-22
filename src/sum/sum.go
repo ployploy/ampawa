fe32b0ada4ae2033f0b7c84d51811fd906be17ab
platylm <n_nareenart@kkumail.com>
2018-09-22 17:02:18 +0700

    [Edited]:เปลี่ยน input ให้เป็นการอ่านไฟล์แทน ของ function Summation

--- src/sum/sum.go
+++ src/sum/sum.go
@@ -1,5 +1,30 @@
 package sum
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func Readfile(path string) []int {
+	var inputs []int
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		number, _ := strconv.Atoi(scanner.Text())
+		inputs = append(inputs, number)
+	}
+
+	return inputs
+
+}
+
 func Summation(input []int) int {
 	var result int
 	for index, _ := range input {
