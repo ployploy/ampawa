bcf00800f1afbd6da2167ee44344317c764b8d7c
ployploy <chonnikan.t@kkumail.com>
2018-09-22 11:45:54 +0700

    [Edited]:ปรับแก้วิธีคิดสำหรับฟังก์ชั่น summation โดยวิธีนี้เป็นการบวกทีละคู่ละเก็บข้อมูลเข้าไปแทนที่ตำแหน่งเดิม

--- src/sum/sum.go
+++ src/sum/sum.go
@@ -2,8 +2,12 @@ package sum
 
 func Summation(input []int) int {
 	var result int
-	for i, _ := range input {
-		result += input[i]
+	for index, _ := range input {
+		if index+1 <= len(input)-1 {
+			input[index+1] = input[index] + input[index+1]
+			result = input[index+1]
+		}
+
 	}
 	return result
 }
--- src/sum/sum_test.go
+++ src/sum/sum_test.go
@@ -137,3 +137,15 @@ func Test_Summation_Input_sum_Should_Be_474872753(t *testing.T) {
 		t.Errorf("expect %d but got it %d", expected, actualResult)
 	}
 }
+
+func Test_Summation_Input_9388053_9957604_7816221_Should_Be_(t *testing.T) {
+	expected := 27161878
+	input := []int{9388053, 9957604, 7816221}
+
+	actualResult := Summation(input)
+
+	if expected != actualResult {
+		t.Errorf("expect %d but got it %d", expected, actualResult)
+	}
+
+}
