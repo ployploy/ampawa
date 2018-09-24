b9956610c24bb8d05dfcc03aa8767dc27b0ae349
ployploy <chonnikan.t@kkumail.com>
2018-09-24 10:01:35 +0700

    [Edited]:เพิ่มให้แสดงทศนิิยมสามตำแหน่ง

--- src/grade_average/gradeaverage_test.go
+++ src/grade_average/gradeaverage_test.go
@@ -9,17 +9,17 @@ func Test_GradeAverage_InputInput_25_4_Should_Be_0(t *testing.T) {
 	actualResult := GradeAverage(inputPoint)
 
 	if expected != actualResult {
-		t.Errorf("expect %3.f but got it %3.f", expected, actualResult)
+		t.Errorf("expect %.3f but got it %.3f", expected, actualResult)
 	}
 }
 
 func Test_GradeAverage_InputInput_ListPoints_Should_Be_1_Dot_375(t *testing.T) {
-	expected := 1.375000
+	expected := 1.375
 	inputPoint := Readfile("inputdata.txt")
 
 	actualResult := GradeAverage(inputPoint)
 
 	if expected != actualResult {
-		t.Errorf("expect %f but got it %f", expected, actualResult)
+		t.Errorf("expect %.3f but got it %.3f", expected, actualResult)
 	}
 }
